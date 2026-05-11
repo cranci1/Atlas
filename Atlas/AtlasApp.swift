641596ba23f60fccde40dc37aadc0fa1f36fb659
cranci1 <100066266+cranci1@users.noreply.github.com>
2026-05-11 16:01:07 +0200

    Initial Commit

---
+++ Atlas/AtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlasApp.swift
+//  Atlas
+//
+//  Created by Francesco on 11/05/26.
+//
+
+import SwiftUI
+
+@main
+struct AtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
